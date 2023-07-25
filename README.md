56770c121903dc6033935f2e3a0d7e8c4622a89c
Coconut <coconutat@protonmail.com>
2023-07-25 14:34:54 +0800

    Add AnyKernel 3

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Xiaomi 845 Kernel for LineageOS 20 with KernelSU
 Kernel Version:4.9.337  
 Support OS:LineageOS 20  
+Compiler:[Proton-Clang](https://github.com/kdrag0n/proton-clang)    
---
+++ tools/AnyKernel3/anykernel.sh
@@ -0,0 +1,41 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=LineageOS 20.0 Mi 8 Series Kernel Mod By Coconutat @ Github
+do.devicecheck=0
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
+'; } # end properties
+
+### AnyKernel install
+# begin attributes
+attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel install
+dump_boot;
+
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+write_boot;
+## end install
