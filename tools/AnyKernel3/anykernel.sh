0fd5c0a0a3206b9a7798924662663dce65c40855
Coconut <coconutat@protonmail.com>
2023-07-27 15:37:16 +0800

    Anykernel 3: Fix name

--- tools/AnyKernel3/anykernel.sh
+++ tools/AnyKernel3/anykernel.sh
@@ -1,15 +1,16 @@
 ### AnyKernel3 Ramdisk Mod Script
 ## osm0sis @ xda-developers
 
-### AnyKernel setup
-# begin properties
 properties() { '
-kernel.string=LineageOS 20.0 Mi 8 Series Kernel Mod By Coconutat @ Github
-do.devicecheck=0
+kernel.string=Xiaomi Mi 8 Explorer Edition Kernel Mod For LineageOS 20 Version -- Kernel MOD & Compile by Coconutat_Ian @ Github
+do.devicecheck=1
 do.modules=0
-do.systemless=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
+device.name1=ursa
+supported.versions=
+supported.patchlevels=
 '; } # end properties
 
 ### AnyKernel install
@@ -20,22 +21,17 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 } # end attributes
 
 
-# shell variables
+## boot shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
-## AnyKernel methods (DO NOT CHANGE)
-# import patching functions/variables - see for reference
-. tools/ak3-core.sh;
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh && attributes;
 
-## AnyKernel install
-dump_boot;
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-# migrate from /overlay to /overlay.d to enable SAR Magisk
-if [ -d $ramdisk/overlay ]; then
-  rm -rf $ramdisk/overlay;
-fi;
-
-write_boot;
-## end install
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
