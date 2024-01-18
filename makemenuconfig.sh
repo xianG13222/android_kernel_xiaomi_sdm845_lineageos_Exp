9c2c5045af4d46ea7aff40751b22718fdf272429
Coconut <coconutat@protonmail.com>
2024-01-18 19:44:51 +0800

    build:build script update

---
+++ makemenuconfig.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#设置环境
+
+# 交叉编译器路径
+export PATH=$PATH:$(pwd)/../Compiler/Zyc_Clang_16/bin
+export CC=clang
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=aarch64-linux-gnu-
+export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+
+export ARCH=arm64
+# export DTC_EXT=dtc
+
+make ARCH=arm64 O=out CC=clang generic_lineageos_ksu_defconfig
+make ARCH=arm64 O=out CC=clang menuconfig
